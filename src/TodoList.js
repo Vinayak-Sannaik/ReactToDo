f18f422fcd18e3e4f1f1bc750b9ec97012bf4dc5
Vinayak Sannaik <vinayakpsannaik@gmail.com>
2023-10-21 12:17:47 +0530

    issue1

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,44 @@
 import { TodoItem } from "./TodoItem";
 
+// export function TodoList({ todos, toggleTodo, deleteTodo }) {
+//   return (
+//     <ul className="list">
+//       {/* {todos.length === 0 && "No Todos"} */}
+//       {todos.map((todo) => {
+//         return (
+//           <TodoItem
+//             id={todo.id}
+//             completed={todo.completed}
+//             title={todo.title}
+//             key={todo.id}
+//             toggleTodo = {toggleTodo}
+//             deleteTodo = {deleteTodo}
+//           />
+//         );
+//       })}
+//     </ul>
+//   );
+// }
+
+
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  if (!todos || todos.length === 0) {
+    return <div>No Todos</div>;
+  }
+
   return (
     <ul className="list">
-      {todos.length === 0 && "No Todos"}
-      {todos.map((todo) => {
-        return (
-          <TodoItem
-            id={todo.id}
-            completed={todo.completed}
-            title={todo.title}
-            key={todo.id}
-            toggleTodo = {toggleTodo}
-            deleteTodo = {deleteTodo}
-          />
-        );
-      })}
+      {todos.map((todo) => (
+        <TodoItem
+          id={todo.id}
+          completed={todo.completed}
+          title={todo.title}
+          key={todo.id}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
     </ul>
   );
+
 }
